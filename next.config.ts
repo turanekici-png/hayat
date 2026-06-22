08c5525cd6adc50f13637481ba4c3283e2aa8d93
Turan ekici <turanekici@gmail.com>
2026-06-22 15:23:19 +0300

    1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+
+  // Prisma kullanıyorsanız standalone deploy'da faydalı olur.
+  outputFileTracingIncludes: {
+    '/*': [
+      './node_modules/.prisma/client/**/*',
+      './node_modules/@prisma/client/**/*',
+      './prisma/**/*'
+    ]
+  },
   reactStrictMode: true,
   allowedDevOrigins: ["10.0.0.183"],
   experimental: {
